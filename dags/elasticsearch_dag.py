9b5629e2899bdea326baf017642af9c699b7ac7b
Kleber <kleber.porto@hotmail.com>
2022-01-22 17:11:46 +0000

    elasticsearch hook

---
+++ dags/elasticsearch_dag.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from plugins.elasticsearch_plugin.hooks.elastic_hook import ElasticHook
+ 
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020,1,1)
+}
+
+def _print_es_info():
+    hook = ElasticHook()
+    print(hook.info())
+
+with DAG('elasticsearch_dag', schedule_interval='@daily',
+    default_args=default_args, catchup=False) as dag:
+
+        print_es_info = PythonOperator(
+            task_id='print_es_info',
+            python_callable=_print_es_info
+        )
